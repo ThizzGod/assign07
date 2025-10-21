7c544be6527e26aa06f8928ac041419fb92de8de
Max Barker <maxbarker26@gmail.com>
2025-10-20 18:40:09 -0800

    Add test stubs

---
+++ BinarySearchTreeTester.java
@@ -0,0 +1,176 @@
+package assign07;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BinarySearchTreeTester {
+	BinarySearchTree<Integer> emptyTree;
+
+	@BeforeEach
+	void setUp() {
+		emptyTree = new BinarySearchTree<Integer>();
+	}
+
+	@Test
+	void testBinarySearchTreeAddNewElement() {
+
+	}
+	
+	@Test
+	void testBinarySearchAddElementExists() {
+		
+	}
+	
+	@Test
+	void testElementsAreOrderedAfterAdd() {
+		
+	}
+	
+	@Test
+	void testAddAllEmptyTree() {
+		
+	}
+	
+	@Test
+	void testAddAllSomeItemsExist() {
+		
+	}
+	
+	@Test
+	void testAddAllNoItemsExist() {
+		
+	}
+	
+	@Test
+	void testAddAllInsertBetween() {
+		
+	}
+	
+	@Test
+	void testClear() {
+		
+	}
+	
+	@Test
+	void testContainsEmptySet() {
+		
+	}
+	
+	@Test
+	void testContainsDoesNotContain() {
+		
+	}
+	
+	@Test
+	void tetsContainsDoesContain() {
+		
+	}
+	
+	@Test
+	void testContainsAllContainsSome() {
+		
+	}
+	
+	@Test
+	void testContainsAllContainsAll() {
+		
+	}
+	
+	@Test
+	void testContainsAllContainsNone() {
+		
+	}
+	
+	@Test
+	void testContainsAllEmptyCollectionAsArgument() {
+		
+	}
+	
+	@Test
+	void testContainsAllEmptySetEmptyCollection() {
+		
+	}
+	
+	@Test
+	void testFirstEmptySet() {
+		
+	}
+	
+	@Test
+	void testFirstReturnsSmallestElement() {
+		
+	}
+	
+	@Test
+	void testIsEmptySetIsNotEmpty() {
+		
+	}
+	
+	@Test
+	void testIsEmptySetIsEmpty() {
+		
+	}
+	
+	@Test
+	void testLastEmptySet() {
+		
+	}
+	
+	@Test
+	void testLastReturnsSmallestElement() {
+		
+	}
+	
+	@Test
+	void testRemoveElementDoesNotExist() {
+		
+	}
+	
+	@Test
+	void testRemoveElementDoesExist() {
+		
+	}
+	
+	@Test
+	void testRemoveEmptySet() {
+		
+	}
+	
+	@Test
+	void testRemoveAllNoElementsExist() {
+		
+	}
+	
+	@Test
+	void testRemoveAllSomeElementsExist() {
+		
+	}
+	
+	@Test
+	void testRemoveAllAllElementsExist() {
+		
+	}
+	
+	@Test
+	void testSizeEmptySet() {
+		
+	}
+	
+	@Test
+	void testSize() {
+		
+	}
+	
+	@Test
+	void testToArrayListEmptySet() {
+		
+	}
+	
+	@Test
+	void testToArrayListIsSorted() {
+		
+	}
+}
+
