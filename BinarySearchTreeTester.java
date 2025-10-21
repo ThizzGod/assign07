3162644b5530eda7e637a28712932d266f9772b4
Max Barker <maxbarker26@gmail.com>
2025-10-20 20:35:42 -0800

    Add tests to tester

--- BinarySearchTreeTester.java
+++ BinarySearchTreeTester.java
@@ -12,6 +12,7 @@ class BinarySearchTreeTester {
 	BinarySearchTree<Integer> zeroToFour;
 	BinarySearchTree<Integer> tenToTwenty; 
 	ArrayList<Integer> listZeroToFour;
+	ArrayList<Integer> emptyList;
 
 	@BeforeEach
 	void setUp() {
@@ -36,7 +37,6 @@ class BinarySearchTreeTester {
 	@Test
 	void testBinarySearchTreeAddNewElement() {
 		assertTrue(emptyTree.add(0));
-		
 	}
 	
 	@Test
@@ -51,51 +51,75 @@ class BinarySearchTreeTester {
 		zeroToFour.add(7);
 		assertEquals(-6, zeroToFour.first());
 		assertEquals(7, zeroToFour.last());
-		
 	}
 	
 	@Test
 	void testAddAllEmptyTree() {
-		emptyTree.addAll(null);
+		assertTrue(emptyTree.addAll(listZeroToFour));
 	}
 	
 	@Test
 	void testAddAllSomeItemsExist() {
-		
+		tenToTwenty.add(1);
+		assertTrue(tenToTwenty.addAll(listZeroToFour));
+		assertEquals(0, tenToTwenty.first());
 	}
 	
 	@Test
 	void testAddAllNoItemsExist() {
-		
+		assertTrue(tenToTwenty.addAll(listZeroToFour));
+		assertEquals(0, tenToTwenty.first());
+	}
+	
+	@Test
+	void testAddAllAllItemsExist() {
+		assertFalse(zeroToFour.addAll(listZeroToFour));
 	}
 	
 	@Test
 	void testAddAllInsertBetween() {
-		
+		tenToTwenty.addAll(listZeroToFour);
+		tenToTwenty.add(7);
+		assertEquals(0, tenToTwenty.first());
+		assertEquals(20, tenToTwenty.last());
+		assertTrue(tenToTwenty.contains(7));
+	}
+	
+	@Test
+	void testAddAllEmptyCollectionAdded() {
+		assertFalse(zeroToFour.addAll(emptyList));
+	}
+	
+	@Test
+	void testAddAllEmptyTreeEmptyCollection() {
+		assertFalse(emptyTree.addAll(emptyList));
 	}
 	
 	@Test
 	void testClear() {
-		
+		tenToTwenty.clear();
+		assertEquals(0, tenToTwenty.size());
 	}
 	
 	@Test
 	void testContainsEmptySet() {
-		
+		assertFalse(emptyTree.contains(5));
 	}
 	
 	@Test
 	void testContainsDoesNotContain() {
-		
+		assertFalse(zeroToFour.contains(10));
 	}
 	
 	@Test
 	void tetsContainsDoesContain() {
-		
+		assertTrue(zeroToFour.contains(4));
 	}
 	
 	@Test
 	void testContainsAllContainsSome() {
+		tenToTwenty.add(1);
+		assertFalse(tenToTwenty.containsAll(listZeroToFour));
 		
 	}
 	
